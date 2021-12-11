bff300f139f5c61c4716bc3a3609aad9301b0ccf
Andrii-25 <kotsupyr.a@gmail.com>
2021-12-11 11:23:05 +0200

    update movies service

--- src/service/moviesService.js
+++ src/service/moviesService.js
@@ -1,19 +1,23 @@
 import http from "../http-common";
 
 class MoviesService {
+  constructor() {
+    this.API_KEY = process.env.REACT_APP_API_KEY;
+    this.LANGUAGE = process.env.REACT_APP_LANGUAGE;
+  }
   getPopularMovies(pageNumber = 1) {
     return http.get(
-      `movie/popular?api_key=${process.env.REACT_APP_API_KEY}&language=${process.env.REACT_APP_LANGUAGE}&page=${pageNumber}`
+      `movie/popular?api_key=${this.API_KEY}&language=${this.LANGUAGE}&page=${pageNumber}`
     );
   }
   getMovie(id) {
     return http.get(
-      `movie/${id}?api_key=${process.env.REACT_APP_API_KEY}&language=${process.env.REACT_APP_LANGUAGE}`
+      `movie/${id}?api_key=${this.API_KEY}&language=${this.LANGUAGE}`
     );
   }
   getVideos(id) {
     return http.get(
-      `movie/${id}/videos?api_key=${process.env.REACT_APP_API_KEY}&language=${process.env.REACT_APP_LANGUAGE}`
+      `movie/${id}/videos?api_key=${this.API_KEY}&language=${this.LANGUAGE}`
     );
   }
 }
