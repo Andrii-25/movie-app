44ceec24b8053217a68062fd06235f1969698d42
Andrii <kotsupyr.a@gmail.com>
2022-03-17 22:33:35 +0200

    add messenger chat

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import "antd/dist/antd.css";
 import "./App.css";
 import Router from "./Router";
+import MessengerCustomerChat from "react-messenger-customer-chat";
 
 function App() {
   return (
     <div className="App">
       <Router />
+      <MessengerCustomerChat
+        pageId="100030477510943"
+        appId="1602695806755817"
+      />
     </div>
   );
 }
