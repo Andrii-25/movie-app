9f4ecf166f44fede5bc4e461f9c81201df9aecda
Andrii <kotsupyr.a@gmail.com>
2022-03-18 20:09:11 +0200

    change messenger chat

--- src/components/AppLayout.js
+++ src/components/AppLayout.js
@@ -1,8 +1,8 @@
 import { Layout } from "antd";
 import { Content } from "antd/lib/layout/layout";
-import { FacebookProvider, MessageUs } from "react-facebook";
 import styled from "styled-components";
 import Header from "./Header";
+import { MessengerChat } from "react-messenger-chat-plugin";
 
 const StyledLayout = styled(Layout)`
   min-height: 100vh;
@@ -29,9 +29,29 @@ export default function AppLayout({ children, isBack }) {
         <Header isBack={isBack} />
         <StyledContent>{children}</StyledContent>
       </StyledLayout>
-      <FacebookProvider appId="143293964389622">
-        <MessageUs messengerAppId="143293964389622" pageId="110809101051972" />
-      </FacebookProvider>
+      <MessengerChat
+        pageId="110809101051972"
+        language="ua_UA"
+        themeColor={"#F2F3G2"}
+        height={24}
+        loggedInGreeting="Hello logged in user!"
+        loggedOutGreeting="Hello stranger!"
+        autoExpand={true}
+        debugMode={false}
+        onMessengerShow={() => {
+          console.log("onMessengerShow");
+        }}
+        onMessengerHide={() => {
+          console.log("onMessengerHide");
+        }}
+        onMessengerDialogShow={() => {
+          console.log("onMessengerDialogShow");
+        }}
+        onMessengerDialogHide={() => {
+          console.log("onMessengerDialogHide");
+        }}
+      />
+      ;
     </>
   );
 }
